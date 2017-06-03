2c2d68ab967ec32cd81d3837499f503f5ec40ee2
Thamyres Corrêa <Thamyres Corra>
2017-06-03 00:26:22 -0300

    My first component!

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,18 @@
-console.log("It Work!s");
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1>Hello Word!</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+render(<App/>, document.getElementById("app"));
